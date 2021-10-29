5e129acf4b9e1dfd3d69236d15e5ea42ba34b24e
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-10-29 18:04:24 -0300

    frontend
    add card description dashboard

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ToastrModule } from "ngx-toastr";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TokenInterceptorService } from "./service/usuario/token-interceptor.service";
-import { AuthGuard } from "./guard/auth.guard";
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { TokenInterceptorService } from "./service/usuario/token-interceptor.service";
+import { AuthGuard } from "./guard/auth.guard";
 
 import { SharedModule } from "./shared/shared.module";
 import { PagesModule } from "./pages/pages.module";
