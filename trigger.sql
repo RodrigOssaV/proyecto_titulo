53d70d7da1b62605a6eb88e0782e655353a487d9
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-11-07 02:00:26 -0300

    Update trigger.sql
    up

--- trigger.sql
+++ trigger.sql
@@ -33,3 +33,7 @@ for each row begin
 	update drivers set total_load = varAmount where drivers.rut = old.rut_driver;
 end;
 $$ DELIMITER ;
+
+INSERT INTO roles VALUES (1, 'user',now(), now());
+INSERT INTO roles VALUES (2, 'moderator',now(), now());
+INSERT INTO roles VALUES (3, 'admin',now(), now());
