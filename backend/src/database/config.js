1d9e06adbd518a7ea3c269829a596d23d8727eb9
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-11-06 21:40:43 -0300

    backend
    Authorization and Roles backend

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -3,6 +3,17 @@ module.exports = {
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: 'dashboard_webapp'
+        database: 'dashboard_webapp',
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
+
+    auth: {
+        secret: "bezkoderAuthor-secret-key"
     }
 }
--- backend/src/database/sequelize.relations.js
+++ backend/src/database/sequelize.relations.js
@@ -7,3 +7,29 @@ Load.belongsTo(Driver, {foreignKey: 'rut_driver' });
 Load.belongsTo(Supplier, {foreignKey: 'id_supplier' });
 
 Status_Load.belongsTo(Load, {foreignKey: 'id_load' });
+
+const Sequelize = require('sequelize');
+const sequelize = require("./db");
+
+const db = {};
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize;
+
+db.user = require("../model/usuario.model")(sequelize, Sequelize);
+db.role = require("../model/role.model")(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+    through: "user_roles",
+    foreignKey: "roleId",
+    otherKey: "userId"
+});
+db.user.belongsToMany(db.role, {
+    through: "user_roles",
+    foreignKey: "userId",
+    otherKey: "roleId"
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
